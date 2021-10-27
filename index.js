07b1ae7980f0d8de46433d9ec213a87fddcbff4c
WillFrame <32850788+WillFrame@users.noreply.github.com>
2021-10-28 02:32:38 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+let profileEditButton = document.querySelector('.profile__edit-button');
+
+
+
+
+profileEditButton.addEventListener ('click', popupOpen);
+formElement.addEventListener('submit', formSubmitHandler);
+profileEditCloseButton.addEventListener('click', popupClose);
