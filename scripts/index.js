eeb386c609acb708c10fce5d242c3c9336761572
WillFrame <32850788+WillFrame@users.noreply.github.com>
2021-10-31 14:50:13 +0300

    Add files via upload

--- scripts/index.js
+++ scripts/index.js
@@ -1,39 +1,30 @@
 let profileEditButton = document.querySelector('.profile__edit-button');
-let formElement = document.querySelector('.popup__save-button');
+let formElement = document.querySelector('.popup__form');
 let profileEditCloseButton = document.querySelector('.popup__close');
 
-function popupOpen() {
-    let popup = document.querySelector('.popup');
-    let profileName = document.querySelector('.profile__name');
-    let profileSubName = document.querySelector('.profile__subname');
-    let inputName = document.querySelector('.popup__input_name');
-    let inputSubName = document.querySelector('.popup__input_subname');
-    /* я знаю, что в задании указаны другие имена, но я уже везде использовал эти */
+let popup = document.querySelector('.popup');
+let profileName = document.querySelector('.profile__name');
+let profileSubName = document.querySelector('.profile__subname');
+let inputName = document.querySelector('.popup__input_content_name');
+let inputSubName = document.querySelector('.popup__input_content_subname');
 
-    popup.classList.toggle('popup_opened');
+function popupOpen() {
     inputName.value = profileName.textContent;
     inputSubName.value = profileSubName.textContent;
+    popup.classList.add('popup_opened');
 }
 
-function formSubmitHandler(evt) {
-    evt.preventDefault();
-    let popup = document.querySelector('.popup');
-    let profileName = document.querySelector('.profile__name');
-    let profileSubName = document.querySelector('.profile__subname');
-    let inputName = document.querySelector('.popup__input_name').value;
-    let inputSubName = document.querySelector('.popup__input_subname').value;
-
-    profileName.textContent = inputName;
-    profileSubName.textContent = inputSubName;
-    popup.classList.toggle('popup_opened');
+function popupSubmit(event) {
+    event.preventDefault();
+    profileName.textContent = inputName.value;
+    profileSubName.textContent = inputSubName.value;
+    popupClose();
 }
 
 function popupClose() {
-    let popup = document.querySelector('.popup');
-    
-    popup.classList.toggle('popup_opened');
+    popup.classList.remove('popup_opened');
 }
 
 profileEditButton.addEventListener('click', popupOpen);
-formElement.addEventListener('submit', formSubmitHandler);
+formElement.addEventListener('submit', popupSubmit);
 profileEditCloseButton.addEventListener('click', popupClose);
